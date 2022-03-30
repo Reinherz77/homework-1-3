109c673516175a6f59ddd1eb3efec8e23ca7e6c3
Naufal Hafiz <hafiz08214@gmail.com>
2022-03-30 16:18:53 +0700

    AUTH SPOTIFY

--- src/App.js
+++ src/App.js
@@ -1,14 +1,47 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import CardSong from './components/card-song';
-import data from './components/data';
+//import data from './components/data';
+//import SearchSong from './components/search-song';
+import axios from 'axios';
+import auth from './auth';
 
 function App() {
+  const [token, setToken] = useState("");
+  const [searchSong, setSearchSong] = useState("");
+  const [songData, setSongData] = useState([]);
+
+  useEffect(() => {
+    const queryString = new URL(window.location.href.replace("#", "?"))
+      .searchParams;
+    const accessToken = queryString.get("access_token");
+    setToken(accessToken);
+  }, []);
+
+  const getSong = async () => {
+    await axios
+      .get(
+        `https://api.spotify.com/v1/search?q=${searchSong}&type=track&access_token=${token}`
+      )
+      .then((response) => {
+        setSongData(response.data.tracks.items);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
   return (
     <div className="App">
       <div className='Container'>
         <div className='SongCard'>
+          <div className='Search'>
+            <button onClick={auth}>Login</button>
+            <input type="search" placeholder='search' onChange={(e) => setSearchSong(e.target.value)} />
+            <button onClick={getSong}>Search</button>
+          </div>
+          
           {
-          data.map(item => {
+          songData.map(item => {
             return(
               <CardSong
                 key={item.id}
